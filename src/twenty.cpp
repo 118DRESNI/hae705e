c4ef53a88d208fbd1a45ad6620ee5c9c878b8ed6
118DRESNI <76472618+118DRESNI@users.noreply.github.com>
2023-09-30 01:18:33 +0200

    ajout addBlock et shiftUp

--- src/twenty.cpp
+++ src/twenty.cpp
@@ -6,26 +6,32 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 #include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 using namespace std;
 
 const int h = 8;const int w = 7;
 int niv[h][w];
-int initval = -1;
+int initval = 0;
 int score = 0;
 int scoreprev = score;
-int difficulty = 0;
+int difficulty = 5;
 
 void initNiv();
 void printNiv();
 //void calcDifficulty(int score);
-void addBlocks(int difficulty);
-
+void addBlocks();
+void shiftUp();
 
 int main() {
+	srand (time(NULL));
 	initNiv();
-	while(){
+	while(1){
+		shiftUp();
+		addBlocks();
 		printNiv();
 		cout << "_______" << endl; // fin de tour
+		cin.get();
 	}
 	return 0; // jamais atteint
 }
@@ -46,6 +52,23 @@ void printNiv(){
         cout << endl;
     }
 }
+
+void addBlocks(){
+	for (int i=0; i <w; i++){
+		niv[h-1][i] = rand() % (difficulty + 1);
+	}
+}
+
+void shiftUp(){
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (i > 0) {
+                niv[i - 1][j] = niv[i][j];
+            }
+        }
+    }
+}
+
 /*
 void calcDifficulty(int score){
 	if (score % 10 == 0 && score > scoreprev){
